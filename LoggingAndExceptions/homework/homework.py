cd6ede2171d2013a1813e6090f2eb7a32166382c
Alex Zasanskyi <o.zasanskyi@ukr.net>
2020-11-01 19:37:02 +0200

    homework7 done

--- LoggingAndExceptions/homework/homework.py
+++ LoggingAndExceptions/homework/homework.py
@@ -1,3 +1,6 @@
+import logging.handlers
+
+
 class Error(Exception):
     pass
 
@@ -11,14 +14,19 @@ class TooFewVisitors(Error):
 
 
 class Concert:
-
     # add 2 class attributes - max_visitors (200) and min_visitors (10)
+    max_visitors = 200
+    min_visitors = 10
 
     def __init__(self, visitors_num):
         """
         if visitors num is bigger than max_visitors - raise TooManyVisitors error
         if visitors num is less than min_visitors - raise TooFewVisitors error
         """
+        if visitors_num > self.max_visitors:
+            raise TooManyVisitors
+        elif visitors_num < self.min_visitors:
+            raise TooFewVisitors
 
 
 def make_concert(visitors_num):
@@ -26,6 +34,23 @@ def make_concert(visitors_num):
     create Concert instance - handle TooManyVisitors and TooFewVisitors errors here:
     in case if caught - log error to console and return False, in case of successful initialization - return True
     """
+    try:
+        a = Concert(9)
+    except TooManyVisitors as e:
+        print(e)
+        return False
+    except TooFewVisitors as e:
+        print(e)
+        return False
+    else:
+        return True
+
+
+logger_obj = logging.getLogger('test')
+logger_obj.setLevel(logging.DEBUG)
+handler = logging.handlers.RotatingFileHandler("test.log", mode='w')
+logger_obj.addHandler(handler)
+
 
 # create Logger object
 # set level to debug
@@ -39,3 +64,13 @@ def log_message(message, level):
     :param message:
     :param level:
     """
+    if level == 10:
+        logger_obj.debug(message)
+    elif level == 20:
+        logger_obj.info(message)
+    elif level == 30:
+        logger_obj.warning(message)
+    elif level == 40:
+        logger_obj.error(message)
+    elif level == 50:
+        logger_obj.critical(message)
